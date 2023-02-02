e0aef590eb1352b27d595fc8c1c8b3169982db94
Brian Choi <89228685+brianchoiJsonar@users.noreply.github.com>
2023-02-01 16:13:31 -0800

    Update main.tf
    testing rds

--- main.tf
+++ main.tf
@@ -14,12 +14,29 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0ba1f6f0ac0fdbc71"
-  instance_type = "r6i.xlarge"
+#resource "aws_instance" "web" {
+#  ami           = "ami-0ba1f6f0ac0fdbc71"
+#  instance_type = "r6i.xlarge"
 
+#  tags = {
+#    Name = "HelloWorld"
+#    Owner = "brian.choi@imperva.com"
+#  }
+  
+ resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "brian-terraform-testing"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+   
   tags = {
-    Name = "HelloWorld"
     Owner = "brian.choi@imperva.com"
   }
+}
+  
 }
